60d0fc7d9a2276148be8d2c3b1fc31603b5877ac
Diego-Andrade <bets636@gmail.com>
2020-10-28 11:04:20 -0700

    Making a skeleton

---
+++ memSim.py
@@ -0,0 +1,32 @@
+import sys
+# CONST
+PAGE_SIZE = 256
+
+# Sys info
+num_frames = 256    #default
+pra = "FIFO"        #default
+
+# Runtime data
+f_addresses = ""
+
+def parse_command_line():
+    pass
+
+def translate(filename):
+    pass
+
+def setup_PRA(pra):
+    pass
+
+def main():
+    parse_command_line()
+    setup_PRA(pra)
+    translate(f_addresses)
+    
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
+
